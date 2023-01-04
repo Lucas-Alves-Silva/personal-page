d82d6308a20510e93ccac32187030e9fd74179fa
Lucas Alves <65175946+Lucas-Alves-Silva@users.noreply.github.com>
2023-01-04 16:01:27 -0300

    Correção no menu responsivo.

--- actions.js
+++ actions.js
@@ -1,13 +1,14 @@
 var visibilidade = true; //Variável que vai manipular o botão Exibir/ocultar
 var mostrar = document.querySelector(".menu");
 var ocultar = document.querySelector(".menu");
+var close = 'https://raw.githubusercontent.com/Lucas-Alves-Silva/personal-page/master/assets/close.png';
 
 function display() { // Quando clicar no botão.
 
   if (visibilidade) {//Se a variável visibilidade for igual a true, então...
       mostrar.style.display = "block";//Mostramos a div..
       visibilidade = false;//alteramos o valor da variável para falso.
-      burguer.src = 'https://raw.githubusercontent.com/Lucas-Alves-Silva/personal-page/master/assets/close.png';
+      burguer.src = close;
   } else {//ou se a variável estiver com o valor false..
       ocultar.style.display = "none";//Ocultamos a div..
       visibilidade = true;//Alteramos o valor da variável para true.
