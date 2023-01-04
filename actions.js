19e3a9f5ef82a789e5ed6ca3268f77978ea41147
Lucas Alves <65175946+Lucas-Alves-Silva@users.noreply.github.com>
2023-01-04 00:43:12 -0300

    Alteração no menu

--- actions.js
+++ actions.js
@@ -1,14 +1,16 @@
 var visibilidade = true; //Variável que vai manipular o botão Exibir/ocultar
 var mostrar = document.querySelector(".menu");
-var ocultar = document.querySelector(".menu")
+var ocultar = document.querySelector(".menu");
 
 function display() { // Quando clicar no botão.
 
   if (visibilidade) {//Se a variável visibilidade for igual a true, então...
       mostrar.style.display = "block";//Mostramos a div..
       visibilidade = false;//alteramos o valor da variável para falso.
+      burguer.src = './assets/close.png';
   } else {//ou se a variável estiver com o valor false..
       ocultar.style.display = "none";//Ocultamos a div..
       visibilidade = true;//Alteramos o valor da variável para true.
+      burguer.src = './assets/menu.png';
   }
 }
